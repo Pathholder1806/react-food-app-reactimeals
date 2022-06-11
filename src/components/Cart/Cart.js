056a6be46077a88c4c615ede9835ce09fd4c0286
Yashjeet Singh <syashjeet08@gmail.com>
2022-06-11 13:59:06 +0530

    Section: Cart -> Order Button -> Added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,47 +6,57 @@ import "./Cart.css";
 import CartContext from "../../store/CartContext";
 
 const Cart = (props) => {
-  const CartCtx = useContext(CartContext);
+    const CartCtx = useContext(CartContext);
 
-  return (
-    <section className="section-cart">
-      <div className="section-cart__cart-div">
-        <div className="section-cart__heading">
-          <div className="section-cart__heading__logo">
-            <h1>Your Cart</h1> <ion-icon name="cart-outline"></ion-icon>
-          </div>
-          <div className="section-cart__heading__btn">
-            <button onClick={CartCtx.cartViewToggler}>
-              <ion-icon name="close-outline"></ion-icon>
-            </button>
-          </div>
-        </div>
-        {CartCtx.numberOfItemsInCart === 0 && (
-          <h1 className="empty-cart-message">No items in cart.</h1>
-        )}
-        {CartCtx.numberOfItemsInCart > 0 && (
-          <div className="section-cart__items">
-            {CartCtx.itemsInCart.map((item) => {
-              return (
-                <CartItem
-                  key={item.title}
-                  title={item.title}
-                  qty={item.qty}
-                  price={item.price}
-                />
-              );
-            })}
-            <div className="cart-total">
-              <span className="cart-total__heading">Cart Total :</span>
-              <span className="cart-total__value">
-                ${CartCtx.cartTotalValue}
-              </span>
+    return (
+        <section className='section-cart'>
+            <div className='section-cart__cart-div'>
+                <div className='section-cart__heading'>
+                    <div className='section-cart__heading__logo'>
+                        <h1>Your Cart</h1>{" "}
+                        <ion-icon name='cart-outline'></ion-icon>
+                    </div>
+                    <div className='section-cart__heading__btn'>
+                        <button onClick={CartCtx.cartViewToggler}>
+                            <ion-icon name='close-outline'></ion-icon>
+                        </button>
+                    </div>
+                </div>
+                {CartCtx.numberOfItemsInCart === 0 && (
+                    <h1 className='empty-cart-message'>No items in cart.</h1>
+                )}
+                {CartCtx.numberOfItemsInCart > 0 && (
+                    <div>
+                        <div className='section-cart__items'>
+                            {CartCtx.itemsInCart.map((item) => {
+                                return (
+                                    <CartItem
+                                        key={item.title}
+                                        title={item.title}
+                                        qty={item.qty}
+                                        price={item.price}
+                                    />
+                                );
+                            })}
+                            <div className='cart-total'>
+                                <span className='cart-total__heading'>
+                                    Cart Total :
+                                </span>
+                                <span className='cart-total__value'>
+                                    ${CartCtx.cartTotalValue}
+                                </span>
+                            </div>
+                        </div>
+                        <div className='section-cart__action'>
+                            <button className='section-cart__action__btn'>
+                                Order
+                            </button>
+                        </div>
+                    </div>
+                )}
             </div>
-          </div>
-        )}
-      </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default Cart;
---
+++ src/components/OrderConfirmation/OrderConfirmation.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+import "./OrderConfirmation.css";
+
+const OrderConfirmation = () => {
+    return <Fragment></Fragment>;
+};
+
+export default OrderConfirmation;
